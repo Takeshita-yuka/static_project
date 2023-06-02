91bb24730867508eaefb4df7146dd38aae5668f4
DESKTOP-Q55MH3L\kgym1 <kgym1807@gmail.com>
2023-06-02 22:42:52 +0900

    基本文法課題03

---
+++ src/main/java/static_project/PracticeFor.java
@@ -0,0 +1,16 @@
+package static_project;
+
+public class PracticeFor {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int sum = 0;
+		for (int num = 1; num <= 100; num++) {
+			sum = sum + num;
+			num = num + 1;
+		}
+		System.out.println(sum);
+	}
+
+}
