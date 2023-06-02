56edcc2493663efe1973ba967480f9f8d233dfb9
DESKTOP-Q55MH3L\kgym1 <kgym1807@gmail.com>
2023-06-02 16:35:53 +0900

    基本文法課題 01

---
+++ src/main/java/static_project/PracticeWhile.java
@@ -0,0 +1,15 @@
+package static_project;
+
+public class PracticeWhile {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int i = 100;
+		while (i >= 0) {
+			System.out.println(i);
+			i--;
+		}
+	}
+
+}
