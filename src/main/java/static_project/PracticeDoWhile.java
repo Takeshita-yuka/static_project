1dff82c6495c219d8142cc7c3d9eae67093b91d4
DESKTOP-Q55MH3L\kgym1 <kgym1807@gmail.com>
2023-06-02 16:42:28 +0900

    基本文法課題02

---
+++ src/main/java/static_project/PracticeDoWhile.java
@@ -0,0 +1,15 @@
+package static_project;
+
+public class PracticeDoWhile {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int i = 100;
+		do {
+			System.out.println(i);
+			i--;
+		} while (i >= 0);
+	}
+
+}
