5a4e461c0ccd6bc4d8b03dccd16d718800afe1ac
DESKTOP-Q55MH3L\kgym1 <kgym1807@gmail.com>
2023-06-03 21:51:56 +0900

    修正いたしました

--- src/main/java/static_project/PracticeForEach.java
+++ src/main/java/static_project/PracticeForEach.java
@@ -5,7 +5,7 @@ public class PracticeForEach {
 	public static void main(String[] args) {
 		
 		String[] list = { "五条", "虎杖", "伏黒", "釘崎", "禪院", "狗巻", "パンダ",
-				"歌姫", "東堂", "加茂", "西宮", "三輪", "メカ丸", "夜蛾", "楽巌寺"};
+				"歌姫", "東堂", "加茂", "西宮", "三輪", "メカ丸", "夜蛾", "楽巌寺" };
 		
 		for (String name: list) {
 			System.out.println(name);
